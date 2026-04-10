547015834e589f72ff3671b90f8e7933d1d3a359
Govind40674 <govindkumar4067444@outlook.com>
2026-04-10 07:34:37 +0530

    change in env

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-
+import dotenv from "dotenv";
+dotenv.config();
 
 
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
