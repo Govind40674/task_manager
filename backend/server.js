1098d9014e0440056f375b5d47caad5da1bc523a
Govind40674 <govindkumar4067444@outlook.com>
2026-04-10 07:38:46 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,19 @@ dotenv.config();
 
 
 const express = require("express");
+
 const cors = require("cors");
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors({
+  origin: "*"
+}));
 
 // Middleware
-app.use(cors());
+
 app.use(express.json());
 
 // In-memory storage
